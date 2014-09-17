e116ee6261590e6e532c8c08aac887742cb9a2c1
Nicholas Corgan <n.corgan@gmail.com>
2014-09-16 21:55:25 -0700

    Changed all instances of BGB to GB

--- gblink-dissector/packet-gblink.c
+++ gblink-dissector/packet-gblink.c
@@ -10,94 +10,94 @@
 #include <wireshark/config.h>
 #include <epan/packet.h>
 
-const unsigned int BGBLINK_PORT = 8765;
+const unsigned int GBLINK_PORT = 8765;
 
-static int proto_bgblink = -1;
+static int proto_gblink = -1;
 
-static int hf_bgblink_b1 = -1; //Command
-static int hf_bgblink_b2 = -1;
-static int hf_bgblink_b3 = -1;
-static int hf_bgblink_b4 = -1;
-static int hf_bgblink_i1 = -1; //Timestamp
+static int hf_gblink_b1 = -1; //Command
+static int hf_gblink_b2 = -1;
+static int hf_gblink_b3 = -1;
+static int hf_gblink_b4 = -1;
+static int hf_gblink_i1 = -1; //Timestamp
 
-static int ett_bgblink = -1;
+static int ett_gblink = -1;
 
 //Forward declaration of dissector functions
-void proto_register_bgblink(void);
-void proto_reg_handoff_bgblink(void);
-static void dissect_bgblink(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
+void proto_register_gblink(void);
+void proto_reg_handoff_gblink(void);
+static void dissect_gblink(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree);
 
-static void dissect_bgblink(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
+static void dissect_gblink(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree)
 {
     gint offset = 0;
     gint len = tvb_reported_length(tvb);
 
-    col_append_str(pinfo->cinfo, COL_PROTOCOL, "BGB");
-    col_append_sep_fstr(pinfo->cinfo, COL_INFO, NULL, "BGB", tvb_format_text_wsp(tvb, 0, len));
+    col_append_str(pinfo->cinfo, COL_PROTOCOL, "GB");
+    col_append_sep_fstr(pinfo->cinfo, COL_INFO, NULL, "GB", tvb_format_text_wsp(tvb, 0, len));
 
     if(tree)
     {
         proto_item* ti = NULL;
-        proto_tree* bgblink_tree = NULL;
+        proto_tree* gblink_tree = NULL;
 
-        ti = proto_tree_add_item(tree, proto_bgblink, tvb, 0, -1, ENC_NA);
-        bgblink_tree = proto_item_add_subtree(ti, ett_bgblink);
+        ti = proto_tree_add_item(tree, proto_gblink, tvb, 0, -1, ENC_NA);
+        gblink_tree = proto_item_add_subtree(ti, ett_gblink);
 
-        proto_tree_add_item(bgblink_tree, hf_bgblink_b1, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(gblink_tree, hf_gblink_b1, tvb, offset, 1, ENC_BIG_ENDIAN);
         offset++;
-        proto_tree_add_item(bgblink_tree, hf_bgblink_b2, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(gblink_tree, hf_gblink_b2, tvb, offset, 1, ENC_BIG_ENDIAN);
         offset++;
-        proto_tree_add_item(bgblink_tree, hf_bgblink_b3, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(gblink_tree, hf_gblink_b3, tvb, offset, 1, ENC_BIG_ENDIAN);
         offset++;
-        proto_tree_add_item(bgblink_tree, hf_bgblink_b4, tvb, offset, 1, ENC_BIG_ENDIAN);
+        proto_tree_add_item(gblink_tree, hf_gblink_b4, tvb, offset, 1, ENC_BIG_ENDIAN);
         offset++;
-        proto_tree_add_item(bgblink_tree, hf_bgblink_i1, tvb, offset, 4, ENC_BIG_ENDIAN);
+        proto_tree_add_item(gblink_tree, hf_gblink_i1, tvb, offset, 4, ENC_BIG_ENDIAN);
     }
 }
 
-void proto_register_bgblink(void)
+void proto_register_gblink(void)
 {
     static hf_register_info hf[] = {
         {
-            &hf_bgblink_b1,
+            &hf_gblink_b1,
             {
-                "B1/Command", "bgblink.b1",
+                "B1/Command", "gblink.b1",
                 FT_UINT8, BASE_DEC,
                 NULL, 0x0,
                 "Byte 1 / Link Command", HFILL
             }
         },
         {
-            &hf_bgblink_b2,
+            &hf_gblink_b2,
             {
-                "B2", "bgblink.b2",
+                "B2", "gblink.b2",
                 FT_UINT8, BASE_HEX,
                 NULL, 0x0,
                 "Byte 2", HFILL
             }
         },
         {
-            &hf_bgblink_b3,
+            &hf_gblink_b3,
             {
-                "B3", "bgblink.b3",
+                "B3", "gblink.b3",
                 FT_UINT8, BASE_HEX,
                 NULL, 0x0,
                 "Byte 3", HFILL
             }
         },
         {
-            &hf_bgblink_b4,
+            &hf_gblink_b4,
             {
-                "B4", "bgblink.b4",
+                "B4", "gblink.b4",
                 FT_UINT8, BASE_HEX,
                 NULL, 0x0,
                 "Byte 4", HFILL
             }
         },
         {
-            &hf_bgblink_i1,
+            &hf_gblink_i1,
             {
-                "I1/Timestamp", "bgblink.i1",
+                "I1/Timestamp", "gblink.i1",
                 FT_UINT32, BASE_DEC,
                 NULL, 0x0,
                 "Timestamp", HFILL
@@ -105,15 +105,15 @@ void proto_register_bgblink(void)
         }
     };
 
-    proto_bgblink = proto_register_protocol("BGB Link Cable Protocol", "BGB Link", "bgblink");
-    proto_register_field_array(proto_bgblink, hf, array_length(hf));
-    register_dissector("bgblink", dissect_bgblink, proto_bgblink);
+    proto_gblink = proto_register_protocol("GB Link Cable Protocol", "GB Link", "gblink");
+    proto_register_field_array(proto_gblink, hf, array_length(hf));
+    register_dissector("gblink", dissect_gblink, proto_gblink);
 }
 
 //Handler registration
-void proto_reg_handoff_bgblink(void)
+void proto_reg_handoff_gblink(void)
 {
-    static dissector_handle_t bgblink_handle;
-    bgblink_handle = create_dissector_handle(dissect_bgblink, bgblink_handle);
-    dissector_add_uint("tcp.port", BGBLINK_PORT, bgblink_handle);
+    static dissector_handle_t gblink_handle;
+    gblink_handle = create_dissector_handle(dissect_gblink, gblink_handle);
+    dissector_add_uint("tcp.port", GBLINK_PORT, gblink_handle);
 }
